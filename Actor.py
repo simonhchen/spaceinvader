f780d21871f6c13018a2bef2f2e0c0e3eb9aa007
Simon Chen <simonhchen@gmail.com>
2020-04-11 16:54:48 -0700

    add Actor.py

---
+++ Actor.py
@@ -0,0 +1,21 @@
+import cocos.sprite
+import cocos.collision_model as cm
+import cocos.euclid as eu
+
+class Actor(cocos.sprite.Sprite):
+    def __init__(self, image, x, y):
+        super(Actor, self).__init__(image)
+        self.position = eu.Vector2(x, y)
+        self.cshape = cm.AARectShape(self.position,
+                                     self.width * 0.5,
+                                     self.height * 0.5)
+
+    def move(self, offset):
+        self.position += offset
+        self.cshape.center += offset
+
+    def update(self, elapsed):
+        pass
+
+    def collide(self, other):
+        pass
