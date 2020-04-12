c16d7d848c3c017816e0f997eed0712f1ee87aa2
Simon Chen <simonhchen@gmail.com>
2020-04-11 18:04:59 -0700

    Ch 2.02 checkpoint commit to SpaceInvader.py

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -8,6 +8,7 @@ import cocos.sprite
 import cocos.collision_model as cm
 import cocos.euclid as eu
 
+
 class Actor(cocos.sprite.Sprite):
     def __init__(self, image, x, y):
         super(Actor, self).__init__(image)
@@ -25,6 +26,7 @@ class Actor(cocos.sprite.Sprite):
 
     def collide(self, other):
         pass
+
 
 class PlayerCannon(Actor):
     KEYS_PRESSED = defaultdict(int)
@@ -44,8 +46,9 @@ class PlayerCannon(Actor):
         other.kill()
         self.kill()
 
+
 class GameLayer(cocos.layer.Layer):
-    is is_event_handler = True
+    is_event_handler = True
 
     def on_key_press(self, k, _):
         PlayerCannon.KEYS_PRESSED[k] = 1
@@ -58,14 +61,14 @@ class GameLayer(cocos.layer.Layer):
         w, h = cocos.director.director.get_window_size()
         self.width = w
         self.height = h
-        self.lives= 3
+        self.lives = 3
         self.score = 0
         self.update_score()
         self.create_player()
         self.create_alien_group(100, 300)
         cell = 1.25 * 50
-        self.collman= cm.CollisionManagerGrid(0, w , 0, h,
-                                              cell, cell)
+        self.collman = cm.CollisionManagerGrid(0, w, 0, h,
+                                               cell, cell)
         self.schedule(self.update)
 
     def create_player(self):
@@ -94,8 +97,88 @@ class GameLayer(cocos.layer.Layer):
                 return True
         return False
 
+    def create_alien_group(self, x, y):
+        self.alien_group = AlienGroup(x, y)
+        for alien in self.alien_group:
+            self.add(alien)
+
+class Alien(Actor):
+    def load_animation(imgage):
+        seq = ImageGrid(load(imgage), 2, 1)
+        return Animation.from_image_sequence(seq, 0.5)
+
+    TYPES = {
+        '1': (load_animation('img/alien1.png'), 40),
+        '2': (load_animation('img/alien2.png'), 20),
+        '3': (load_animation('img/alien3.png'), 10)
+    }
+
+    def from_type(x, y, alien_type, column):
+        animation, score = Alien.TYPES[alien_type]
+        return Alien(animation, x, y, score, column)
+
+    def __init__(self, img, x, y, score, column=None):
+        super(Alien, self).__init__(img, x, y)
+        self.score = score
+        self.column = column
+
+    def on_exit(self):
+        super(Alien, self).on_exit()
+        if self.column:
+            self.column.remove(self)
+
+
+class AlienColumn(object):
+    def __init__(self, x, y):
+        alien_types = enumerate(['3', '3', '2', '2', '1'])
+        self.aliens = [Alien.from_type(x, y + i * 60, alien, self)
+                       for i, alien in alien_types]
+
+    def should_turn(self, d):
+        if len(self.aliens) == 0:
+            return False
+        alien = self.aliens[0]
+        x, width = alien.x, alien.parent.width
+        return x >= width - 50 and d == 1 or x <= 50 and d == -1
+
+    def remove(self, alien):
+        self.aliens.remove(alien)
+
+    def shoot(self): pass
+
+
+class AlienGroup(object):
+    def __init__(self, x, y):
+        self.columns = [AlienColumn(x + i * 60, y)
+                        for i in range(10)]
+        self.speed = eu.Vector2(10,0)
+        self.direction = 1
+        self.elapsed = 0.0
+        self.period = 1.0
+
+    def update(self, elapsed):
+        self.elapsed += elapsed
+        while self.elapsed >= self.period:
+            self.elapsed -= self.period
+            offset = self.direction * self.speed
+            if self.side_reached():
+                self.direction *= -1
+                offset = eu.Vector2(0, -10)
+            for alien in self:
+                alien.move(offset)
+
+    def side_reached(self):
+        return any(map(lambda c: c.should_turn(self.direction),
+                       self.columns))
+
+    def __iter__(self):
+        for column in self.columns:
+            for alien in column.aliens:
+                yield alien
+
+
 if __name__ == '__main__':
-    cocos.director.director.init(captions="Space Invaders",
+    cocos.director.director.init(caption="Space Invaders",
                                  width=800, height=650)
     game_layer = GameLayer()
     main_scene = cocos.scene.Scene(game_layer)
